4136c1b7cbad74cc38047dc1a5648887af31a768
Sekidde Derrick <kidricederek@gmail.com>
2019-03-14 17:11:09 +0300

    Add reset page.[finishes #164604288]

--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -1,5 +1,5 @@
 function checkAdminEmail(){
-
+// function checks for email value and redirects to appropriate email
     let response = document.getElementById("email").value;
     if (response == "admin@epic-mail.com"){
         location = "admin.html";
@@ -8,4 +8,8 @@ function checkAdminEmail(){
         location = "user.html";
     }
     return false;
+}
+function resetPassword(){
+    //confirmaton for reset password
+    alert("Link has been sent to your email");
 }
